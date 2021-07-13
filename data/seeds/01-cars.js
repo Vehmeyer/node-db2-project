aa5a068cee8cb8cb645f57296521f3b2f26d962f
Vehmeyer <j.vehmeyer@gmail.com>
2021-07-13 15:52:56 -0500

    created db seed

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,10 @@
-// STRETCH
+exports.seed = function (knex) {
+    return knex('cars').truncate()
+        .then(function () {
+            return knex('cars').insert([
+                {vin: '1FTHW25F4TEB51909', make: 'Jeep', model: 'Wrangler', mileage: 50000, title: 'clean', transmission: 'working'},
+                {vin: '2GKALSEKXC6183960', make: 'Dodge', model: 'Dart', mileage: 50000, title: 'lien', transmission: ''},
+                {vin: '1GT22XEG2FZ167918', make: 'Chevy', model: 'Nova', mileage: 50000, title: '', transmission: ''},
+            ])
+        })
+}
