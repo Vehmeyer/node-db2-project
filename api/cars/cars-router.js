6bd7cdff7161626bba83fadffe94b5a3b80acf94
Vehmeyer <j.vehmeyer@gmail.com>
2021-07-13 13:35:26 -0500

    wip

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,22 @@
-// DO YOUR MAGIC
+const router = require('express').Router()
+const Car = require('./cars-model')
+const md = require('./cars-middleware')
+
+router.get('/', (req, res, next) => {
+    console.log('GET all connected')
+})
+
+router.get('/:id', (req, res, next) => {
+    console.log('GET by id connected')
+})
+router.post('/', (req, res, next) => {
+    console.log('POST connected')
+})
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      message: err.message,
+      stack: err.stack,
+    })
+  })
+
+  module.exports = router;
